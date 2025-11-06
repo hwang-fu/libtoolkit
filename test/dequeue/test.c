abbcc27c475d09138b590a37ab8188826c02edb9
hwang-fu <junzhe.hwangfu@gmail.com>
2025-11-06 11:45:56 +0100

    test(dequeue): test case update

--- test/dequeue/test.c
+++ test/dequeue/test.c
@@ -3,11 +3,22 @@
 
     {
         Dequeue * dq = mk_dq(0);
-        ASSERT_EXPR(NEQ(dq, NIL));
+        if (EQ(dq, NIL))
+        {
+            fail(cases);
+        }
         dq_dispose(dq);
         pass(cases++);
     }
 
     {
+        Dequeue * dq = mk_dq(0);
+        dq_pushback(dq, 1);
+        dq_pushback(dq, 2);
+        dq_pushback(dq, 3);
+        dq_pushback(dq, 4);
+        dq_pushback(dq, 5);
+        dq_dispose(dq);
+        pass(cases++);
     }
 }
