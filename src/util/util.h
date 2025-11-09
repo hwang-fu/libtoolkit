bbd583746e477b8e49a7d43da01c4c42d31f19ab
hwang-fu <junzhe.hwangfu@gmail.com>
2025-11-09 11:18:04 +0100

    docs(util): doc

--- src/util/util.h
+++ src/util/util.h
@@ -1,1 +1,24 @@
 #pragma once
+
+#include <hwangfu/generic.h>
+
+/**
+ * @since       09.11.2025
+ * @author      Junzhe
+ * @modified    09.11.2025
+ */
+u64 GreatestCommonDivisor(u64 a, u64 b);
+
+/**
+ * @since       09.11.2025
+ * @author      Junzhe
+ * @modified    09.11.2025
+ */
+u64 LeastCommonMultiple(u64 a, u64 b);
+
+/**
+ * @since       09.11.2025
+ * @author      Junzhe
+ * @modified    09.11.2025
+ */
+OWNED u64 * SieveEratosthenes(u64 upto);
