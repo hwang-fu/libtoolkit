dc32e137fa5f5383bc4d101e35feca6df3b09446
hwang-fu <junzhe.hwangfu@gmail.com>
2025-11-09 19:03:06 +0100

    test(s): initial test

--- test/s/test.c
+++ test/s/test.c
@@ -1,2 +1,24 @@
 {
+    printf("Testing module " CRAYON_TO_BOLD(CRAYON_TO_YELLOW("s")) "...\n");
+
+    u64 cases = 1;
+
+    {
+        OWNED char * s = strdup_safe("abcdef");
+        ASSERT_EXPR(strcmp_safe(s, "abcdef"));
+        ASSERT_EXPR(strncmp_safe(s, "a", 1));
+        ASSERT_EXPR(strncmp_safe(s, "ab", 2));
+        ASSERT_EXPR(strncmp_safe(s, "abc", 3));
+        ASSERT_EXPR(strncmp_safe(s, "abcd", 4));
+        ASSERT_EXPR(strncmp_safe(s, "abcde", 5));
+        ASSERT_EXPR(strncmp_safe(s, "abcdef", 6));
+        dispose(s);
+        pass(cases++);
+    }
+
+    {
+    }
+
+    {
+    }
 }
