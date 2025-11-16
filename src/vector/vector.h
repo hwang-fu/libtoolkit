e8310b077bea638548af0b60927e0bcff3285311
hwang-fu <junzhe.hwangfu@gmail.com>
2025-11-16 22:03:57 +0100

    feat(vector): vector structure init

--- src/vector/vector.h
+++ src/vector/vector.h
@@ -1,2 +1,26 @@
 #pragma once
 
+#include <hwangfu/generic.h>
+#include <hwangfu/assertion.h>
+#include <hwangfu/memory.h>
+#include <hwangfu/result.h>
+
+#ifndef VECTOR_DEFAULT_CAPACITY
+#define VECTOR_DEFAULT_CAPACITY (20)
+#endif // VECTOR_DEFAULT_CAPACITY
+
+typedef struct Vector Vector;
+typedef struct VectorData VectorData ;
+
+/**
+ * @since       16.11.2025
+ * @author      Junzhe
+ * @modified    16.11.2025
+ */
+struct Vector
+{
+    copied   u64            Capacity;
+    copied   u64            Size;
+    owned    VectorItem **  Items;
+};
+
