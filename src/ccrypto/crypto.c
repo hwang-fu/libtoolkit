fbcfe60f018d5ea146130859f2919756d23f0809
hwang-fu <junzhe.hwangfu@gmail.com>
2025-11-24 08:46:37 +0100

    feat(crypto): init

---
+++ src/ccrypto/crypto.c
@@ -0,0 +1,20 @@
+#include "crypto.h"
+
+
+
+
+OWNED SHA256 * mk_sha256(BORROWED void * body, u64 bytes)
+{
+}
+
+OWNED char * sha256_cstring(BORROWED SHA256 * h)
+{
+}
+
+OWNED char * sha256_cstring_owned(OWNED SHA256 * h)
+{
+}
+
+OWNED char * sha256_display(FILE * stream, BORROWED SHA256 * h)
+{
+}
